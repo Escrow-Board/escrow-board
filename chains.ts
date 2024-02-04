9005e70907979cc1506bee270758319fbbece823
prakashharvani <prakashharvani1997@gmail.com>
2024-02-04 16:00:38 +0530

    Changed New LightLink network

--- chains.ts
+++ chains.ts
@@ -60,166 +60,20 @@ export const CHAINS: { [chainId: number]: BasicChainInformation | ExtendedChainI
       'https://cloudflare-eth.com',
     ].filter((url) => url),
     name: 'Mainnet',
-  },
-  3: {
-    urls: [process.env.NEXT_PUBLIC_INFURA_KEY ? `https://ropsten.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` : ''].filter(
-      (url) => url
-    ),
-    name: 'Ropsten',
-  },
-  4: {
-    urls: [process.env.NEXT_PUBLIC_INFURA_KEY ? `https://rinkeby.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` : ''].filter(
-      (url) => url
-    ),
-    name: 'Rinkeby',
-  },
-  5: {
-    urls: [process.env.NEXT_PUBLIC_INFURA_KEY ? `https://goerli.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` : ''].filter(
-      (url) => url
-    ),
-    name: 'Goerli',
-  },
-  42: {
-    urls: [process.env.NEXT_PUBLIC_INFURA_KEY ? `https://kovan.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` : ''].filter(
-      (url) => url
-    ),
-    name: 'Kovan',
-  },
-  // Optimism
-  10: {
+  }
+ ,
+  1891: {
     urls: [
-      process.env.NEXT_PUBLIC_INFURA_KEY ? `https://optimism-mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` : '',
-      'https://mainnet.optimism.io',
-    ].filter((url) => url),
-    name: 'Optimism',
-    nativeCurrency: ETH,
-    blockExplorerUrls: ['https://optimistic.etherscan.io'],
-  },
-  69: {
-    urls: [
-      process.env.NEXT_PUBLIC_INFURA_KEY ? `https://optimism-kovan.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` : '',
-      'https://kovan.optimism.io',
-    ].filter((url) => url),
-    name: 'Optimism Kovan',
-    nativeCurrency: ETH,
-    blockExplorerUrls: ['https://kovan-optimistic.etherscan.io'],
-  },
-  // Arbitrum
-  42161: {
-    urls: [
-      process.env.NEXT_PUBLIC_INFURA_KEY ? `https://arbitrum-mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` : '',
-      'https://arb1.arbitrum.io/rpc',
-    ].filter((url) => url),
-    name: 'Arbitrum One',
-    nativeCurrency: ETH,
-    blockExplorerUrls: ['https://arbiscan.io'],
-  },
-  421611: {
-    urls: [
-      process.env.NEXT_PUBLIC_INFURA_KEY ? `https://arbitrum-rinkeby.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` : '',
-      'https://rinkeby.arbitrum.io/rpc',
-    ].filter((url) => url),
-    name: 'Arbitrum Testnet',
-    nativeCurrency: ETH,
-    blockExplorerUrls: ['https://testnet.arbiscan.io'],
-  },
-  // Polygon
-  137: {
-    urls: [
-      process.env.NEXT_PUBLIC_INFURA_KEY ? `https://polygon-mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` : '',
-      'https://polygon-rpc.com',
-    ].filter((url) => url),
-    name: 'Polygon Mainnet',
-    nativeCurrency: MATIC,
-    blockExplorerUrls: ['https://polygonscan.com'],
-  },
-  80001: {
-    urls: [process.env.NEXT_PUBLIC_INFURA_KEY ? `https://polygon-mumbai.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}` : ''].filter(
-      (url) => url
-    ),
-    name: 'Polygon Mumbai',
-    nativeCurrency: MATIC,
-    blockExplorerUrls: ['https://mumbai.polygonscan.com'],
-  },
-  31337: {
-    urls: ['http://localhost:8545'],
-    name: 'Localhost 8545',
-    nativeCurrency: ETH,
-    blockExplorerUrls: undefined,
-  },
-  // Binance
-  56: {
-    urls: [
-      'https://bsc-dataseed.binance.org',
+      'https://replicator.pegasus.lightlink.io/rpc/v1',
     ],
-    name: 'Binance Smart Chain',
+    name: 'Light Link testnet',
     nativeCurrency: {
-      name: 'BNB',
-      symbol: 'BNB',
-      decimals: 18
-    },
-    blockExplorerUrls: ['https://bscscan.com'],
-  },
-  97: {
-    urls: [
-      'https://data-seed-prebsc-1-s1.binance.org:8545/',
-    ],
-    name: 'Binance Smart Chain - Testnet',
-    nativeCurrency: {
-      name: 'BNB',
-      symbol: 'BNB',
-      decimals: 18
-    },
-    blockExplorerUrls: ['https://testnet.bscscan.com'],
-  },
-  // Fantom
-  250: {
-    urls: [
-      'https://rpc.ankr.com/fantom',
-    ],
-    name: 'Fantom Opera',
-    nativeCurrency: {
-      name: 'FTM',
-      symbol: 'FTM',
-      decimals: 18
-    },
-    blockExplorerUrls: ['https://ftmscan.com'],
-  },
-  4002: {
-    urls: [
-      'https://rpc.testnet.fantom.network',
-    ],
-    name: 'Fantom testnet',
-    nativeCurrency: {
-      name: 'FTM',
-      symbol: 'FTM',
-      decimals: 18
-    },
-    blockExplorerUrls: ['https://testnet.ftmscan.com'],
-  },
-  1663: {
-    urls: [
-      'https://gobi-testnet.horizenlabs.io/ethv1',
-    ],
-    name: 'Horizen Gobi Testnet',
-    nativeCurrency: {
-      name: 'Gobi',
-      symbol: 'tZEN',
+      name: 'LETH',
+      symbol: 'LETH',
       decimals: 18
     }
   },
-  1001:{
-    urls: [
-      'https://public-en-baobab.klaytn.net',
-    ],
-    name: 'Klayton Testnet',
-    nativeCurrency: {
-      name: 'KLAY',
-      symbol: 'KLAYt',
-      decimals: 18
-    },
-    blockExplorerUrls: ['https://klayton.com'],
-  },
+  
   
   // https://public-en-baobab.klaytn.net
 }
